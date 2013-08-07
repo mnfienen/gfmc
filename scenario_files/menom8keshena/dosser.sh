de5c7e9c68785c86ccdacb1f58013dccc540288c
Michael Fienen <mnfienen@usgs.gov>
2013-08-07 13:24:22 -0500

    Added a little shell script to each scenario_files subdirectory that runs todos.exe on all relevant files types. This file (dosser.sh) should be included in all scenario_files in the future.

---
+++ scenario_files/menom8keshena/dosser.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+wine ~/bin/todos *.tpl
+wine ~/bin/todos *.ins
+wine ~/bin/todos *.pst
+wine ~/bin/todos *.bat
+wine ~/bin/todos *.tab
+wine ~/bin/todos *.in
+wine ~/bin/todos *.nam
+wine ~/bin/todos *.NAM
+ 
