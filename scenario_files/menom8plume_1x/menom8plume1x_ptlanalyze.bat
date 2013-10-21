2bf3a630c2b07df21a7e67105222941759992e69
Paul <pfjuckem@usgs.gov>
2013-10-21 15:12:30 -0500

    updated batch files to run python from python27 dir.

--- scenario_files/menom8plume_1x/menom8plume1x_ptlanalyze.bat
+++ scenario_files/menom8plume_1x/menom8plume1x_ptlanalyze.bat
@@ -1,6 +1,6 @@
-python GFLOW_ptl_analyzer3.py menom8plume1x_10x.nam
-python GFLOW_ptl_analyzer3.py menom8plume1x_5x.nam
-python GFLOW_ptl_analyzer3.py menom8plume1x_2x.nam
-python GFLOW_ptl_analyzer3.py menom8plume1x_1x.nam
-python GFLOW_ptl_analyzer3.py menom8plume1x_0_5x.nam
-python GFLOW_ptl_analyzer3.py menom8plume1x_0_2x.nam
+..\Python27\python GFLOW_ptl_analyzer3.py menom8plume1x_10x.nam
+..\Python27\python GFLOW_ptl_analyzer3.py menom8plume1x_5x.nam
+..\Python27\python GFLOW_ptl_analyzer3.py menom8plume1x_2x.nam
+..\Python27\python GFLOW_ptl_analyzer3.py menom8plume1x_1x.nam
+..\Python27\python GFLOW_ptl_analyzer3.py menom8plume1x_0_5x.nam
+..\Python27\python GFLOW_ptl_analyzer3.py menom8plume1x_0_2x.nam
